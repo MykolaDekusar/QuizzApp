1df81c0f787ba674c9303318543abeee2855c3be
MykolaDekusar <ndekusar97@gmail.com>
2025-07-21 18:50:45 +0200

    Initial setup

--- QuizzApp/Program.cs
+++ QuizzApp/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿//Objective: create questions with relative answers, if the user answers correctly add a point, show the final result
+string question1 = "How many months are is a year?";
+string question2 = "How many fingers are in a hand?";
+string question3 = "If you close your eyes what do you see?";
+string question4 = "What is the fastest running animal on earth?";
+int answer1 = 12;
+int answer2 = 5;
+string answer3 = "Nothing";
+string answer4 = "Ghepard";
+
+//Let's set the global score
+
+int score = 0;
