de2f856cfc5bac788d4b2cf945e8e97ee2d54e9f
MykolaDekusar <ndekusar97@gmail.com>
2025-07-21 18:59:10 +0200

    Added simple logic for user input to start the quiz game

--- QuizzApp/Program.cs
+++ QuizzApp/Program.cs
@@ -11,3 +11,20 @@ string answer4 = "Ghepard";
 //Let's set the global score
 
 int score = 0;
+
+char startGame;
+
+Console.WriteLine("Let's play a quitz game, if you're ready type \'y', if you're not type \'n'");
+startGame = Console.ReadKey().KeyChar;
+
+if(startGame == 'n')
+{
+    Console.WriteLine("\nWell let's play when you'll be ready!!!");
+} else if(startGame == 'y')
+{
+    Console.WriteLine("\nAllright, let's begin!");
+
+} else
+    Console.WriteLine("\nSorry wrong input type \'y', if you're ready or \'n' if you're not");
+
+Console.ReadKey();
